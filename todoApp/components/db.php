33f3651b9ef40f8a3356bd4794aebb550418a670
Lenonkoech <kipyegonlenon226@gmail.com>
2024-03-29 18:45:13 +0300

    updated database settings

--- todoApp/components/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "todoapp";
-$conn = new mysqli($host, $user, $password, $dbname);
-if (!$conn) {
-    return "Error connecting to database";
-}
--- todoapp.sql
+++ todoapp.sql
@@ -1,232 +1,139 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `todoapp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `todoapp`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: 127.0.0.1:3310
--- Generation Time: Mar 29, 2024 at 02:07 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE
-= "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone
-= "+00:00";
-
+-- Host: 127.0.0.1    Database: todoapp
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `todoapp`
---
-CREATE DATABASE
-IF NOT EXISTS `todoapp` DEFAULT CHARACTER
-SET utf8mb4
-COLLATE utf8mb4_general_ci;
-USE `todoapp`;
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin`
-(
-  `id` int
-(11) NOT NULL,
-  `Name` varchar
-(70) DEFAULT NULL,
-  `username` varchar
-(70) DEFAULT NULL,
-  `Email` varchar
-(256) DEFAULT NULL,
-  `password` varchar
-(256) DEFAULT NULL
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(70) DEFAULT NULL,
+  `username` varchar(70) DEFAULT NULL,
+  `Email` varchar(256) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `categories`
 --
 
-CREATE TABLE `categories`
-(
-  `id` int
-(11) NOT NULL,
-  `category` varchar
-(256) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`
-id`,
-`category
-`) VALUES
-(1, 'personal'),
-(2, 'shopping'),
-(3, 'work'),
-(4, 'errands'),
-(5, 'projects');
-
--- --------------------------------------------------------
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'personal'),(2,'shopping'),(3,'work'),(4,'errands'),(5,'projects');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tasks`
 --
 
-CREATE TABLE `tasks`
-(
-  `id` int
-(11) NOT NULL,
-  `user_id` int
-(11) DEFAULT NULL,
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
   `start_date` date DEFAULT NULL,
   `end_date` date DEFAULT NULL,
-  `description` varchar
-(400) DEFAULT NULL,
-  `category` varchar
-(200) DEFAULT NULL,
-  `Status` varchar
-(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `description` varchar(400) DEFAULT NULL,
+  `category` varchar(200) DEFAULT NULL,
+  `Status` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`
-id`,
-`user_id
-`, `start_date`, `end_date`, `description`, `category`, `Status`) VALUES
-(2, 1, '2023-02-03', '2023-02-23', 'Hello world', 'personal', 'pending'),
-(3, 1, '2024-03-27', '2024-03-29', 'Finish bash scripting', 'Work', 'pending'),
-(4, 1, '2024-03-29', '2024-03-29', 'Buying groceries', 'Shopping', 'pending'),
-(5, 1, '2024-03-28', '2024-04-06', 'Create a to-do-list App', 'Projects', 'pending'),
-(6, 1, '2024-05-31', '2024-04-05', 'Pay house rent', 'Errands', 'pending'),
-(10, 1, '2024-03-28', '2024-04-01', 'Create employee pay slips', 'Work', 'pending'),
-(11, 1, '2024-03-26', '2024-03-27', 'Do homework', 'personal', 'complete');
-
--- --------------------------------------------------------
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (2,1,'2023-02-03','2023-02-23','Hello world','personal','pending'),(3,1,'2024-03-27','2024-03-29','Finish bash scripting','Work','pending'),(4,1,'2024-03-29','2024-03-29','Buying groceries','Shopping','pending'),(5,1,'2024-03-28','2024-04-06','Create a to-do-list App','Projects','pending'),(6,1,'2024-05-31','2024-04-05','Pay house rent','Errands','pending'),(10,1,'2024-03-28','2024-04-01','Create employee pay slips','Work','pending'),(11,1,'2024-03-26','2024-03-27','Do homework','personal','complete');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users`
-(
-  `id` int
-(11) NOT NULL,
-  `Name` varchar
-(70) DEFAULT NULL,
-  `username` varchar
-(70) DEFAULT NULL,
-  `Email` varchar
-(256) DEFAULT NULL,
-  `password` varchar
-(256) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(70) DEFAULT NULL,
+  `username` varchar(70) DEFAULT NULL,
+  `Email` varchar(256) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`
-id`,
-`Name
-`, `username`, `Email`, `password`) VALUES
-(1, 'user', 'user', 'user@gmail.com', '1234');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'user','user','user@gmail.com','1234');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-ADD PRIMARY KEY
-(`id`),
-ADD UNIQUE KEY `username`
-(`username`);
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-ADD PRIMARY KEY
-(`id`);
-
---
--- Indexes for table `tasks`
---
-ALTER TABLE `tasks`
-ADD PRIMARY KEY
-(`id`),
-ADD KEY `user_id`
-(`user_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-ADD PRIMARY KEY
-(`id`),
-ADD UNIQUE KEY `username`
-(`username`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `tasks`
---
-ALTER TABLE `tasks`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tasks`
---
-ALTER TABLE `tasks`
-ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY
-(`user_id`) REFERENCES `users`
-(`id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-29 18:28:42
